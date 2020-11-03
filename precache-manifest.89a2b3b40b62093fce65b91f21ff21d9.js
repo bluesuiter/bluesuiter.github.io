5844c9616460ede98061a19c018bd893e7aa7115
chauhan1991 <amit.chauhan@ethoswatches.com>
2020-11-03 18:20:00 +0530

    first commit

---
+++ precache-manifest.89a2b3b40b62093fce65b91f21ff21d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301d5e2cab994833fc8203888eeaf2ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "297175bff9212e1edbe4",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ca2dd6b8f738b5f307d8",
+    "url": "/static/css/main.43d9b066.chunk.css"
+  },
+  {
+    "revision": "297175bff9212e1edbe4",
+    "url": "/static/js/2.366c6cc5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.366c6cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2dd6b8f738b5f307d8",
+    "url": "/static/js/main.0a627c91.chunk.js"
+  },
+  {
+    "revision": "f5e337750815ed0ba0e7",
+    "url": "/static/js/runtime-main.a0044592.js"
+  },
+  {
+    "revision": "cba429d9358689b7c8d187ea54cef76f",
+    "url": "/static/media/profile.cba429d9.jpg"
+  }
+]);
